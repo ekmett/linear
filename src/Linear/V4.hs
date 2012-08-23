517c3567da4e8928bc6afc949551791f451387de
Anthony Cowley <acowley@gmail.com>
2012-08-22 21:37:18 -0400

    Added Distributive instance for V4.

--- src/Linear/V4.hs
+++ src/Linear/V4.hs
@@ -10,6 +10,7 @@ module Linear.V4
 import Control.Applicative
 import Control.Lens
 import Data.Data
+import Data.Distributive
 import Data.Foldable
 import Data.Monoid
 import Linear.Epsilon
@@ -51,6 +52,9 @@ instance Fractional a => Fractional (V4 a) where
 
 instance Metric V4 where
   dot (V4 a b c d) (V4 e f g h) = a * e + b * f + c * g + d * h
+
+instance Distributive V4 where
+  distribute f = V4 (fmap (^._x) f) (fmap (^._y) f) (fmap (^._z) f) (fmap (^._w) f)
 
 class R3 t => R4 t where
   _w :: Functor f => (a -> f a) -> t a -> f (t a)
