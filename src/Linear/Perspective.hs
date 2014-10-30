0f050cd1838c935c0d699203f23e08c39c5abf7b
Edward Kmett <ekmett@gmail.com>
2014-10-30 02:44:21 -0400

    style matching, copyright date match

--- src/Linear/Perspective.hs
+++ src/Linear/Perspective.hs
@@ -1,6 +1,6 @@
 ---------------------------------------------------------------------------
 -- |
--- Copyright   :  (C) 2012-2013 Edward Kmett,
+-- Copyright   :  (C) 2014 Edward Kmett
 -- License     :  BSD-style (see the file LICENSE)
 --
 -- Maintainer  :  Edward Kmett <ekmett@gmail.com>
@@ -25,16 +25,16 @@ import Linear.Metric
 -- | Build a look at view matrix
 lookAt :: (Epsilon a, Floating a) => V3 a -> V3 a -> V3 a -> M44 a
 lookAt eye center up =
-  V4 (V4 (xa ^. _x)     (xa ^. _y)     (xa ^. _z)     xd)
-     (V4 (ya ^. _x)     (ya ^. _y)     (ya ^. _z)     yd)
-     (V4 (-(za ^. _x))  (-(za ^. _y))  (-(za ^. _z))  zd)
-     (V4 0              0              0              1)
+  V4 (V4 (xa^._x)  (xa^._y)  (xa^._z)  xd)
+     (V4 (ya^._x)  (ya^._y)  (ya^._z)  yd)
+     (V4 (-za^._x) (-za^._y) (-za^._z) zd)
+     (V4 0         0         0          1)
   where za = normalize $ center - eye
         xa = normalize $ cross za up
         ya = cross xa za
-        xd = -(dot xa eye)
-        yd = -(dot ya eye)
-        zd = (dot za eye)
+        xd = -dot xa eye
+        yd = -dot ya eye
+        zd = dot za eye
 
 -- | Build a matrix for a symmetric perspective-view frustum
 perspective :: Floating a => a -> a -> a -> a -> M44 a
@@ -57,11 +57,10 @@ infinitePerspective fovy aspect near =
      (V4 0 0 (-1) w)
      (V4 0 0 (-1) 0)
   where range  = (tan (fovy / 2)) * near
-        left   = (-range) * aspect
+        left   = -range * aspect
         right  = range * aspect
         bottom = -range
         top    = range
         x = (2 * near) / (right - left)
         y = (2 * near) / (top - bottom)
-        w = (-2) * near
-
+        w = -2 * near
