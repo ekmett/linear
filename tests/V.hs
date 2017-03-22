f538e85e9bafc9fe5f51961dd871ed300b5a6695
Ryan Scott <ryan.gl.scott@gmail.com>
2017-03-22 11:08:06 -0400

    Only expose V if GHC >= 7.7

--- tests/V.hs
+++ tests/V.hs
@@ -1,24 +1,13 @@
-{-# LANGUAGE CPP #-}
-
-#if __GLASGOW_HASKELL__ >= 707
 {-# LANGUAGE DataKinds #-}
-#endif
 module V (tests) where
 
-import Test.HUnit
-
-#if __GLASGOW_HASKELL__ >= 707
 import Control.DeepSeq (rnf)
 import qualified Data.Vector.Unboxed as U (fromList)
 import Linear.V (V)
+import Test.HUnit
 
 v10 :: V 10 Int
 v10 = return 5
-#endif
 
 tests :: Test
-tests = test [
-#if __GLASGOW_HASKELL__ >= 707
-               "GH124" ~: rnf (U.fromList [v10]) ~?= ()
-#endif
-             ]
+tests = test [ "GH124" ~: rnf (U.fromList [v10]) ~?= () ]
