822d556ebf139e0044b464cf1723b9df86bef49c
Ryan Scott <ryan.gl.scott@gmail.com>
2017-03-23 10:13:00 -0400

    Add regression test for #124 (#125)
    * Add regression test for #124
    
    * Fix some CPP
    
    * Only expose V if GHC >= 7.7

---
+++ tests/V.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE DataKinds #-}
+module V (tests) where
+
+import Control.DeepSeq (rnf)
+import qualified Data.Vector.Unboxed as U (fromList)
+import Linear.V (V)
+import Test.HUnit
+
+v10 :: V 10 Int
+v10 = return 5
+
+tests :: Test
+tests = test [ "GH124" ~: rnf (U.fromList [v10]) ~?= () ]
