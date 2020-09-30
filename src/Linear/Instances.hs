d7bc72ace66b45ccfd44dec25e3637b4ddba95f1
Ryan Scott <ryan.gl.scott@gmail.com>
2020-09-30 15:14:44 -0400

    Use base-orphans-0.8.3's orphan MonadFix/MonadZip instances for Complex (#157)
    `base-orphans-0.8.3` now backports the `MonadFix` and `MonadZip`
    instances for `Complex` (newly added to `base-4.15.0.0`) to older
    versions of `base`. As a result, we no longer need to define these
    instances as orphans in `Linear.Instances`, which allows us to turn
    that module into a simple shim on top of `Data.Orphans`.

--- src/Linear/Instances.hs
+++ src/Linear/Instances.hs
@@ -1,8 +1,4 @@
-{-# OPTIONS_GHC -fno-warn-orphans #-}
-{-# LANGUAGE CPP #-}
-#if __GLASGOW_HASKELL__ >= 702 && __GLASGOW_HASKELL__ < 710
-{-# LANGUAGE Trustworthy #-}
-#endif
+{-# LANGUAGE Safe #-}
 -----------------------------------------------------------------------------
 -- |
 -- Copyright   :  (C) 2012-2015 Edward Kmett
@@ -11,18 +7,8 @@
 -- Stability   :  provisional
 -- Portability :  portable
 --
--- Orphans
+-- Re-exports orphan instances for @Complex@ from the @base-orphans@ package.
 -----------------------------------------------------------------------------
 module Linear.Instances () where
 
-import Control.Applicative
-import Control.Monad.Fix
-import Control.Monad.Zip
-import Data.Complex
 import Data.Orphans ()
-
-instance MonadZip Complex where
-  mzipWith = liftA2
-
-instance MonadFix Complex where
-  mfix f = (let a :+ _ = f a in a) :+ (let _ :+ a = f a in a)
