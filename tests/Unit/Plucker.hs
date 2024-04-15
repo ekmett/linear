e044271e0085f0aa04895c939de798a4cd413a8a
Ryan Scott <ryan.gl.scott@gmail.com>
2024-04-15 18:02:14 -0400

    Refactor: Generalize naming conventions in test suite
    This is in preparation for adding non-unit tests in a subsequent commit.

--- tests/Unit/Plucker.hs
+++ tests/Unit/Plucker.hs
@@ -1,4 +1,4 @@
-module Plucker (tests) where
+module Unit.Plucker (tests) where
 import Linear
 import Linear.Plucker
 import Linear.Plucker.Coincides
@@ -16,19 +16,19 @@ ln9 = Plucker 1 2 3 4 5 6                   -- not a 3D line
 
 tests :: Test
 tests = test [ "parallel" ~: parallel ln2 ln3 ~?= True
-             , "CCW" ~: passes ln2 ln4 ~?= Counterclockwise 
+             , "CCW" ~: passes ln2 ln4 ~?= Counterclockwise
              , "CW" ~: passes ln2 ln5 ~?= Clockwise
-             , "intersect1" ~: intersects ln2 ln6 ~?= True 
+             , "intersect1" ~: intersects ln2 ln6 ~?= True
              , "intersect2" ~: intersects ln2 ln3 ~?= False
-             , "line equality 1" ~: Line ln2 == Line ln2 ~?= True 
-             , "line equality 2" ~: Line ln2 == Line ln7 ~?= True 
+             , "line equality 1" ~: Line ln2 == Line ln2 ~?= True
+             , "line equality 2" ~: Line ln2 == Line ln7 ~?= True
              , "line equality 3" ~: Line ln2 == Ray ln7 ~?= True
              , "line equality 4" ~: Ray ln2 == Line ln7 ~?= True
              , "ray equality 1" ~: Ray ln2 == Ray ln7 ~?= False
              , "ray equality 2" ~: Ray ln2 == Ray (3 *^ ln2) ~?= True
              , "ray equality 3" ~: Ray ln2 == Ray (negate ln7) ~?= True
              , "quadrance" ~: nearZero (quadranceToOrigin ln2 - 10) ~?= True
-             , "closest 1" ~: 
+             , "closest 1" ~:
                  nearZero (qd (V3 1 3 0) $ closestToOrigin ln2) ~?= True
              , "closest 2" ~: nearZero (qd 0 $ closestToOrigin ln8) ~?= True
              , "isLine 1" ~: isLine ln2 ~?= True
